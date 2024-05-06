9d2b768c3fb087afed76ba0e1d0835b7d59282fe
shubhamghimire99 <shubham0011ghimire@gmail.com>
2024-05-06 23:11:58 +0545

    footer changed

--- components/main/Navbar.tsx
+++ components/main/Navbar.tsx
@@ -6,7 +6,7 @@ const Navbar = () => {
     return (
         <div className='w-full h-[65px] fixed top-0 shadow-lg shadow-[#2A0E61]/50 
         bg-[#03001417] backdrop-blur-md z-50 px-10 '>
-            <div className='w-full h-full flex flex-row items-center justify-between m-auto px-[10px]'>
+            <div className='container mx-auto w-auto flex flex-row items-center justify-between'>
                 <a href="#about-me" className='h-auto w-auto flex flex-row items-center'>
                     <Image src="/NavLogo.png" alt="logo" width={70} height={70} className='cursor-poiner hover:animate-slowspin' />
                     <span className='font-bold ml-[10px] hidden md:block text-gray-300'>
